7ecb63e477cfee0657dc7f7be1167173cbcbff8d
wrgalvao <wrgalvao@unifesspa.edu.br>
2023-05-10 20:20:12 -0300

    PROJETO FINALIZADO

--- js/geracaoSenha.js
+++ js/geracaoSenha.js
@@ -1,13 +1,36 @@
-let tamanhoSenha = 12
+let tamanhoSenha = 11
 let campoTextoSenha = document.querySelector("#texto-senha")
 let botaoGerarSenha = document.querySelector("#botao-gerar-senha")
 botaoGerarSenha.addEventListener("click", geraSenha)
-
+document.querySelector("#senha-tamanho-texto").innerText = tamanhoSenha
+const indicadorSegurancaBarra = document.querySelector("#indicador-seguranca-bar")
+let selecaoMaiuscula = document.querySelector("#btncheck1")
+let selecaoSimbolo = document.querySelector("#btncheck2")
+let selecaoNumerico = document.querySelector("#btncheck3")
 function geraSenha()
 {
-    let caracteres = "abcdefghjkmnpqrstuvwxyzABCDEFGHJKLMNPQRSTUVWXYZ123456789?!@&*()[]"
+    let caracteres = "abcdefghjkmnpqrstuvwxyz"
+    const maisuculas = "ABCDEFGHJKLMNPQRSTUVWXYZ"
+    const simbolos = "?!@&*()[]"
+    const numeros = "123456789"
     let senha = ""
-    //console.log(tamanhoSenha.value)
+
+    if(selecaoMaiuscula.checked)
+    {
+        caracteres += maisuculas
+        console.log(caracteres)
+    }
+    if(selecaoNumerico.checked)
+    {
+        caracteres += numeros
+        console.log(caracteres)
+    }
+    if(selecaoSimbolo.checked)
+    {
+        caracteres += simbolos
+        console.log(caracteres)
+    }
+    
     for(let i = 0; i < tamanhoSenha; i++)
     {
         let indiceAleatorio = Math.floor(Math.random() * caracteres.length)
@@ -15,12 +38,49 @@ function geraSenha()
     }
     console.log(senha)
     campoTextoSenha.value = senha
+    calculaQualidadeSenha()
+}
+
+function calculaQualidadeSenha()
+{
+    /*
+        Pesos para calculo de qualidade: 
+        Tamanho: 25%
+        Maiusculas: 15%
+        Numerico: 25%
+        Simbolos: 35%
+        total = 100%
+    */
+    let porcentagem = Math.round((tamanhoSenha / 16) * 100 * 0.25 + (selecaoMaiuscula.checked ? 15 : 0) + (selecaoNumerico.checked ? 25 : 0) + (selecaoSimbolo.checked ? 35 : 0))
+    if(porcentagem > 0 && porcentagem <= 33)
+    {
+        indicadorSegurancaBarra.classList.remove("sucesso")
+        indicadorSegurancaBarra.classList.remove("aviso")
+        indicadorSegurancaBarra.classList.add("critico")
+        //critico
+    }
+     else if(porcentagem > 33 && porcentagem <= 66)
+    {
+        //aviso = yelow
+        indicadorSegurancaBarra.classList.remove("critico")
+        indicadorSegurancaBarra.classList.remove("sucesso")
+        indicadorSegurancaBarra.classList.add("aviso")
+    }
+    else
+    {
+        indicadorSegurancaBarra.classList.remove("aviso")
+        indicadorSegurancaBarra.classList.remove("critico")
+        indicadorSegurancaBarra.classList.add("sucesso")
+        //safe demais
+    }
+    console.log(porcentagem)
+    indicadorSegurancaBarra.style.width = `${porcentagem}%`
 }
 
 function getTamanhoSenha()
 {
     tamanhoSenha = tamanhoSenhaElemento.value
-    console.log(tamanhoSenha)
+    document.querySelector("#senha-tamanho-texto").innerText = tamanhoSenha
 }
 
 function copiarSenha()
